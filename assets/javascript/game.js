b35693a6649b62f25ec9f94de1b0dcdcd4ccf17f
iamjink <jinijinjin@gmail.com>
2020-01-20 23:34:10 -0800

    pseudocoding in javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,100 +1,123 @@
 $(document).ready(function () {
 
-            //random number generator
 
-            var RandomNumber = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
+    //click function that sends character image to your jedi field and the rest images to enemies to attack field
 
-            //assigning a hidden random value to each crystal between 1 and 12
+    //click function that sends button clicked to your opponent field
 
-            var buttonOne = Math.floor(Math.random() * 12) + 1;
-            var buttonTwo = Math.floor(Math.random() * 12) + 1;
-            var buttonThree = Math.floor(Math.random() * 12) + 1;
-            var buttonFour = Math.floor(Math.random() * 12) + 1;
+    //function for random number is assigned in your jedi
 
-            //setting variables for scores
-            var wins = 0;
-            var losses = 0;
-            var userTotal = 0;
+    //function for random number is assigned in your opponent
 
-            $("#randomNumber").text(RandomNumber);
+    //click function for the attack button. when this button is clicked , and opponet random number decreases by jedi random number. score number shows the random number that is assigned to each of jedi and opponent
 
-            //defining functions for scoreboard
-            function winScore() {
-                wins++;
-                $("#wins").text(wins);
-            }
+    //health point decreases by click and random number
 
-            function lossScore() {
-                losses++;
-                $("#losses").text(losses)
+    //if jedi health point goes to 0, get an alert that says you lost. 
 
-            }
-
-            //click functions that random assign number to each button and add these numbers when clicked
-            $("#button-one").click(() => {
-                userTotal = userTotal + buttonOne;
-                $("#userNumber").text(userTotal);
-                if (userTotal === RandomNumber) {
-                    winScore();
-                    reset();
-                } else if (userTotal > RandomNumber) {
-                    lossScore();
-                    reset();
-                }
-            });
-
-            $("#button-two").click(() => {
-                userTotal = userTotal + buttonTwo;
-                $("#userNumber").text(userTotal);
-                if (userTotal === RandomNumber) {
-                    winScore();
-                    reset();
-                } else if (userTotal > RandomNumber) {
-                    lossScore();
-                    reset();
-                }
-            });
-
-            $("#button-three").click(() => {
-                userTotal = userTotal + buttonThree;
-                $("#userNumber").text(userTotal);
-                if (userTotal === RandomNumber) {
-                    winScore();
-                    reset();
-                } else if (userTotal > RandomNumber) {
-                    lossScore();
-                    reset();
-                }
-            });
-
-            $("#button-four").click(() => {
-                userTotal = userTotal + buttonFour;
-                $("#userNumber").text(userTotal);
-                if (userTotal === RandomNumber) {
-                    winScore();
-                    reset();
-                } else if (userTotal > RandomNumber) {
-                    lossScore();
-                    reset();
-                }
-            });
+    //if oppoment health point goes to 0, get an alert that says you win. 
 
 
 
 
-            //game reset function when win or loss score goes up by 1
-            function reset() {
-                userTotal = 0;
-                $("#userNumber").text(userTotal);
-
-                RandomNumber = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
-                $("#randomNumber").text(RandomNumber);
 
 
-                buttonOne = Math.floor(Math.random() * 12) + 1;
-                buttonTwo = Math.floor(Math.random() * 12) + 1;
-                buttonThree = Math.floor(Math.random() * 12) + 1;
-                buttonFour = Math.floor(Math.random() * 12) + 1;
-            }
 
-        });
+    //random number generator
+
+    var RandomNumber = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
+
+    //assigning a hidden random value to each crystal between 1 and 12
+
+    var buttonOne = Math.floor(Math.random() * 12) + 1;
+    var buttonTwo = Math.floor(Math.random() * 12) + 1;
+    var buttonThree = Math.floor(Math.random() * 12) + 1;
+    var buttonFour = Math.floor(Math.random() * 12) + 1;
+
+    //setting variables for scores
+    var wins = 0;
+    var losses = 0;
+    var userTotal = 0;
+
+    $("#randomNumber").text(RandomNumber);
+
+    //defining functions for scoreboard
+    function winScore() {
+        wins++;
+        $("#wins").text(wins);
+    }
+
+    function lossScore() {
+        losses++;
+        $("#losses").text(losses)
+
+    }
+
+    //click functions that random assign number to each button and add these numbers when clicked
+    $("#button-one").click(() => {
+        userTotal = userTotal + buttonOne;
+        $("#userNumber").text(userTotal);
+        if (userTotal === RandomNumber) {
+            winScore();
+            reset();
+        } else if (userTotal > RandomNumber) {
+            lossScore();
+            reset();
+        }
+    });
+
+    $("#button-two").click(() => {
+        userTotal = userTotal + buttonTwo;
+        $("#userNumber").text(userTotal);
+        if (userTotal === RandomNumber) {
+            winScore();
+            reset();
+        } else if (userTotal > RandomNumber) {
+            lossScore();
+            reset();
+        }
+    });
+
+    $("#button-three").click(() => {
+        userTotal = userTotal + buttonThree;
+        $("#userNumber").text(userTotal);
+        if (userTotal === RandomNumber) {
+            winScore();
+            reset();
+        } else if (userTotal > RandomNumber) {
+            lossScore();
+            reset();
+        }
+    });
+
+    $("#button-four").click(() => {
+        userTotal = userTotal + buttonFour;
+        $("#userNumber").text(userTotal);
+        if (userTotal === RandomNumber) {
+            winScore();
+            reset();
+        } else if (userTotal > RandomNumber) {
+            lossScore();
+            reset();
+        }
+    });
+
+
+
+
+    //game reset function when win or loss score goes up by 1
+    function reset() {
+        userTotal = 0;
+        $("#userNumber").text(userTotal);
+
+        RandomNumber = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
+        $("#randomNumber").text(RandomNumber);
+
+
+        buttonOne = Math.floor(Math.random() * 12) + 1;
+        buttonTwo = Math.floor(Math.random() * 12) + 1;
+        buttonThree = Math.floor(Math.random() * 12) + 1;
+        buttonFour = Math.floor(Math.random() * 12) + 1;
+    }
+
+});
