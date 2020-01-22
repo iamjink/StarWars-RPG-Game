2ec6831f94ade2145c1fae62ec9579b1176d0b9e
iamjink <jinijinjin@gmail.com>
2020-01-22 15:52:59 -0800

    defining reset function in javaScript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,98 @@
 $(document).ready(function () {
 
+    function reset() {
+        window.gameObj = {
+            //attack button to false, then set it to true
+            attackOccured: false,
+            winOccured: false,
+            lossOccured: false,
+            wounded: false,
+            gameOver: false,
+            jediMaster: false,
+
+            //character list array 
+            characterList: [{
+                    name: 'Rey',
+                    visual: 'assets/images/CHARACTER-1.png',
+                    healthPoints: 130,
+                    attackPower: 15,
+                    counterAttackPower: 30,
+                },
+
+                {
+                    name: 'Chewbacca',
+                    visual: 'assets/images/CHARACTER-2.png',
+                    healthPoints: 170,
+                    attackPower: 20,
+                    counterAttackPower: 25,
+                },
+
+                {
+                    name: 'Poe Dameron',
+                    visual: 'assets/images/CHARACTER-3.png',
+                    healthPoints: 130,
+                    attackPower: 12,
+                    counterAttackPower: 25,
+                },
+
+                {
+                    name: 'Yoda',
+                    visual: 'assets/images/CHARACTER-4.png',
+                    healthPoints: 130,
+                    attackPower: 45,
+                    counterAttackPower: 60,
+                },
+
+                {
+                    name: 'Darth Vader',
+                    visual: 'assets/images/CHARACTER-5.png',
+                    healthPoints: 180,
+                    attackPower: 25,
+                    counterAttackPower: 30,
+                },
+
+                {
+                    name: 'Kylo Ren',
+                    visual: 'assets/images/CHARACTER-6.png',
+                    healthPoints: 150,
+                    attackPower: 18,
+                    counterAttackPower: 22,
+                },
+            ],
+
+            //initializes game start
+            gameStart: true,
+            //initializes Jedi to nothing
+            userCharacter: null,
+            //initializes enemy to nothing
+            currentEnemy: null,
+            //initializes array of previously fought enemies
+            previousEnemy: [],
+            //sets current attack power to nothing
+            currentAttackPower: null,
+
+            //array of battle sounds
+            battleSoundArray: ['assets/audio/saberclash.mp3', 'assets/audio/saberclash1.mp3', 'assets/audio/saberclash2.mp3', 'assets/audio/saberclash3.mp3', 'assets/audio/saberclash4.mp3', 'assets/audio/saberclash5.mp3', 'assets/audio/saberclash6.mp3', 'assets/audio/spin1.mp3', 'assets/audio/spin2.mp3', 'assets/audio/spin3.mp3', 'assets/audio/spin4.mp3', 'assets/audio/spin5.mp3', 'assets/audio/spin6.mp3', 'assets/audio/swing1.mp3', 'assets/audio/swing2.mp3', ],
+            jediSelectSound: 'assets/audio/saberon.mp3',
+
+            //generation random battle sound from array when attack button is pressed
+            RandomBattleSound: function () {
+                return this.battleSoundArray[Math.floor(Math.random() * this.battleSoundArray.length)];
+            },
+        }
+    };
+
+    //game start
+    reset();
+
+    function play() {
+        //defining jQuery object of variables
+        var $jediList = $('#jediChoices');
+        var $enemyList = $('#enemyChoices');
+
+    }
+
+
 
     //click function that sends character image to your jedi field and the rest images to enemies to attack field
 
